80b70bb3f8f6ae2a9569abbee21637269916439c
pushkraj <dev.pushkraj@gmail.com>
2023-07-03 15:20:44 +0530

    day1

---
+++ Day 1 - 73. Set Matrix Zeroes.cpp
@@ -0,0 +1,31 @@
+// Author Pushkraj
+// Date 3 july 2023
+// Q 73. Set Matrix Zeroes
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+// You must do it in place.
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int i,j,n,m,k,col0=1;
+        n = matrix.size();
+        m = matrix[0].size();
+        for(i=0; i<n; i++){
+            if(matrix[i][0] == 0) col0=0;
+            for(j=1; j<m; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for(i=n-1; i>=0; i--){
+            for(j=m-1; j>0; j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+            if(col0==0) matrix[i][0] = 0;
+        }
+    }
+};
+
